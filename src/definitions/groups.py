637d8148c72b14c3a738728527a6c0d7359bf6d0
victor <vpoten@gmail.com>
2022-03-07 15:31:55 +0100

    first commit

---
+++ src/definitions/groups.py
@@ -0,0 +1,134 @@
+FRESHDESK_GROUPS = [
+    {
+        "id": 14000111243,
+        "name": "Billing",
+        "description": None,
+        "escalate_to": None,
+        "unassigned_for": None,
+        "business_hour_id": None,
+        "group_type": "support_agent_group",
+        "created_at": "2018-11-26T12:47:45Z",
+        "updated_at": "2021-04-12T07:15:09Z"
+    },
+    {
+        "id": 14000114323,
+        "name": "Customer Care",
+        "description": None,
+        "escalate_to": None,
+        "unassigned_for": None,
+        "business_hour_id": None,
+        "group_type": "support_agent_group",
+        "created_at": "2021-04-27T15:35:35Z",
+        "updated_at": "2021-04-27T15:35:35Z"
+    },
+    {
+        "id": 14000115233,
+        "name": "Dev-A-Team",
+        "description": "The A-Team, equipo Dev. (antiguo \"backend\")",
+        "escalate_to": None,
+        "unassigned_for": None,
+        "business_hour_id": None,
+        "group_type": "support_agent_group",
+        "created_at": "2022-02-17T10:55:39Z",
+        "updated_at": "2022-02-17T10:56:18Z"
+    },
+    {
+        "id": 14000115230,
+        "name": "Dev-Avengers",
+        "description": "Equipo desarrollo Avengers.",
+        "escalate_to": None,
+        "unassigned_for": None,
+        "business_hour_id": None,
+        "group_type": "support_agent_group",
+        "created_at": "2022-02-17T10:52:02Z",
+        "updated_at": "2022-02-17T10:53:13Z"
+    },
+    {
+        "id": 14000115231,
+        "name": "Dev-Gondor",
+        "description": "Equipo Dev Gondor. (Antiguo \"Frontend\")",
+        "escalate_to": None,
+        "unassigned_for": None,
+        "business_hour_id": None,
+        "group_type": "support_agent_group",
+        "created_at": "2022-02-17T10:53:33Z",
+        "updated_at": "2022-02-17T10:57:55Z"
+    },
+    {
+        "id": 14000115232,
+        "name": "Dev-Mordor",
+        "description": "Equipo Dev Mordor.",
+        "escalate_to": None,
+        "unassigned_for": None,
+        "business_hour_id": None,
+        "group_type": "support_agent_group",
+        "created_at": "2022-02-17T10:54:46Z",
+        "updated_at": "2022-02-17T10:55:11Z"
+    },
+    {
+        "id": 14000115234,
+        "name": "Dev-Plugins",
+        "description": "The Plugin Stones, equipo Dev.",
+        "escalate_to": None,
+        "unassigned_for": None,
+        "business_hour_id": None,
+        "group_type": "support_agent_group",
+        "created_at": "2022-02-17T10:56:36Z",
+        "updated_at": "2022-02-17T10:57:19Z"
+    },
+    {
+        "id": 14000114272,
+        "name": "Devs - BE",
+        "description": None,
+        "escalate_to": None,
+        "unassigned_for": None,
+        "business_hour_id": None,
+        "group_type": "support_agent_group",
+        "created_at": "2021-04-05T08:12:53Z",
+        "updated_at": "2021-04-12T07:14:57Z"
+    },
+    {
+        "id": 14000114271,
+        "name": "Devs - FE",
+        "description": None,
+        "escalate_to": None,
+        "unassigned_for": None,
+        "business_hour_id": None,
+        "group_type": "support_agent_group",
+        "created_at": "2021-04-05T08:12:11Z",
+        "updated_at": "2021-04-12T07:14:48Z"
+    },
+    {
+        "id": 14000115229,
+        "name": "Devs-Triage",
+        "description": "Los tickets asignados a este grupo tienen que ser revisados por Desarrollo, y están pendientes de asignar a un equipo Dev.",
+        "escalate_to": None,
+        "unassigned_for": None,
+        "business_hour_id": None,
+        "group_type": "support_agent_group",
+        "created_at": "2022-02-17T10:48:35Z",
+        "updated_at": "2022-02-17T10:51:29Z"
+    },
+    {
+        "id": 14000111924,
+        "name": "FirstResponders",
+        "description": "Doofinder's finest.",
+        "escalate_to": None,
+        "unassigned_for": None,
+        "business_hour_id": None,
+        "group_type": "support_agent_group",
+        "created_at": "2019-05-13T15:26:36Z",
+        "updated_at": "2021-04-12T07:14:33Z"
+    },
+    {
+        "id": 14000114815,
+        "name": "Internal",
+        "description": None,
+        "escalate_to": None,
+        "unassigned_for": None,
+        "business_hour_id": None,
+        "group_type": "support_agent_group",
+        "created_at": "2021-11-04T16:46:23Z",
+        "updated_at": "2021-11-04T16:46:45Z"
+    }
+]
