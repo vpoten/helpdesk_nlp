551805969e45e02e7fed45b21baf12dd6590a68e
victor <vpoten@gmail.com>
2022-03-10 16:18:16 +0100

    group classes

---
+++ src/definitions/group_class.py
@@ -0,0 +1,82 @@
+from enum import Enum
+
+
+class TicketClasses(Enum):
+    rest = 0
+    billing = 1
+    customer_care = 2
+    development = 3
+    dev_ateam = 4
+    dev_avengers = 5
+    dev_gondor = 6
+    dev_mordor = 7
+    dev_plugins = 8
+    dev_be = 9
+    dev_fe = 10
+    dev_triage = 11
+    first_resp = 12
+    internal = 13
+
+
+CLASS_DIRECT = {
+    "Billing": TicketClasses.billing,
+    "Customer Care": TicketClasses.customer_care,
+    "Dev-A-Team": TicketClasses.dev_ateam,
+    "Dev-Avengers": TicketClasses.dev_avengers,
+    "Dev-Gondor": TicketClasses.dev_gondor,
+    "Dev-Mordor": TicketClasses.dev_mordor,
+    "Dev-Plugins": TicketClasses.dev_plugins,
+    "Devs - BE": TicketClasses.dev_be,
+    "Devs - FE": TicketClasses.dev_fe,
+    "Devs-Triage": TicketClasses.dev_triage,
+    "FirstResponders": TicketClasses.first_resp,
+    "Internal": TicketClasses.internal,
+}
+
+# L1 is development vs. rest
+CLASS_L1 = {
+    "Billing": TicketClasses.rest,
+    "Customer Care": TicketClasses.rest,
+    "Dev-A-Team": TicketClasses.development,
+    "Dev-Avengers": TicketClasses.development,
+    "Dev-Gondor": TicketClasses.development,
+    "Dev-Mordor": TicketClasses.development,
+    "Dev-Plugins": TicketClasses.development,
+    "Devs - BE": TicketClasses.development,
+    "Devs - FE": TicketClasses.development,
+    "Devs-Triage": TicketClasses.development,
+    "FirstResponders": TicketClasses.rest,
+    "Internal": TicketClasses.rest,
+}
+
+# L2 is dev_be, dev_fe vs. rest
+CLASS_L2 = {
+    "Billing": TicketClasses.rest,
+    "Customer Care": TicketClasses.rest,
+    "Dev-A-Team": TicketClasses.dev_be,
+    "Dev-Avengers": TicketClasses.dev_fe,
+    "Dev-Gondor": TicketClasses.dev_fe,
+    "Dev-Mordor": TicketClasses.dev_fe,
+    "Dev-Plugins": TicketClasses.dev_fe,
+    "Devs - BE": TicketClasses.dev_be,
+    "Devs - FE": TicketClasses.dev_fe,
+    "Devs-Triage": TicketClasses.rest,
+    "FirstResponders": TicketClasses.rest,
+    "Internal": TicketClasses.rest,
+}
+
+# L3 is dev_ateam, dev_avengers, dev_gondor, dev_mordor, dev_plugins, dev_fe vs. rest
+CLASS_L3 = {
+    "Billing": TicketClasses.rest,
+    "Customer Care": TicketClasses.rest,
+    "Dev-A-Team": TicketClasses.dev_ateam,
+    "Dev-Avengers": TicketClasses.dev_avengers,
+    "Dev-Gondor": TicketClasses.dev_gondor,
+    "Dev-Mordor": TicketClasses.dev_mordor,
+    "Dev-Plugins": TicketClasses.dev_plugins,
+    "Devs - BE": TicketClasses.dev_ateam,
+    "Devs - FE": TicketClasses.dev_fe,
+    "Devs-Triage": TicketClasses.rest,
+    "FirstResponders": TicketClasses.rest,
+    "Internal": TicketClasses.rest,
+}
--- src/freshdesk/assign_features.py
+++ src/freshdesk/assign_features.py
@@ -1,4 +1,5 @@
 import export_loader as export_loader
+import src.definitions.group_class as group_class
 from src.nlp.language_detector import detect_language
 
 ID = export_loader.ID
@@ -7,42 +8,42 @@ STATUS = export_loader.STATUS
 
 
 def get_class1(group):
-    # TODO
-    return None
+    return group_class.CLASS_L1.get(group)
 
 
 def get_class2(group):
-    # TODO
-    return None
+    return group_class.CLASS_L2.get(group)
 
 
 def get_class3(group):
-    # TODO
-    return None
+    return group_class.CLASS_L3.get(group)
 
 
 def get_language(conversation):
-    text = None if not conversation else conversation[0].get('body_text', None)
-    return None if not text else detect_language(text)
+    try:
+        text = None if not conversation else conversation[0].get('body_text', None)
+        return None if not text else detect_language(text)
+    except Exception as err:
+        return None
 
 
-def get_clean_text(conversation):
-    # TODO
-    return None
+def get_conversation_text(conversation):
+    return None if not conversation else conversation[0].get('body_text', None)
 
 
 def _build_features(ticket_id, row):
-    group = row[GROUP]
-    status = row[STATUS]
+    group = row.iloc[0][GROUP]
+    status = row.iloc[0][STATUS]
     conversation = export_loader.load_conversation(ticket_id)
     language = get_language(conversation)
-    text = get_clean_text(conversation)
+    text = get_conversation_text(conversation)
     c1 = get_class1(group)
     c2 = get_class2(group)
     c3 = get_class3(group)
     return {
         'id': ticket_id,
         'status': status,
+        'group': group,
         'language': language,
         'text': text,
         'c1': c1,
