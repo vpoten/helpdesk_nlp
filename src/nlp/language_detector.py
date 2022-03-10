5184f833866bfb8825ea075e30360974df7a12d6
victor <vpoten@gmail.com>
2022-03-10 14:18:27 +0100

    language detector

---
+++ src/nlp/language_detector.py
@@ -0,0 +1,10 @@
+from langdetect import detect
+
+
+def detect_language(text):
+    return detect(text)
+
+
+if __name__ == "__main__":
+    print(detect_language("A user-defined generic class can have ABCs as base classes without a metaclass conflict"))
+    print(detect_language("Las tropas aliadas han alcanzado sus ultimos objetivos"))
