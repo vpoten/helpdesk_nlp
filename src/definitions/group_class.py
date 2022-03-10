a1ddc759c1e97e5d63f39685b47fa44bf72da685
victor <vpoten@gmail.com>
2022-03-10 20:00:55 +0100

    cleaning 2

--- src/definitions/group_class.py
+++ src/definitions/group_class.py
@@ -2,20 +2,20 @@ from enum import Enum
 
 
 class TicketClasses(Enum):
-    rest = '0'
-    billing = '1'
-    customer_care = '2'
-    development = '3'
-    dev_ateam = '4'
-    dev_avengers = '5'
-    dev_gondor = '6'
-    dev_mordor = '7'
-    dev_plugins = '8'
-    dev_be = '9'
-    dev_fe = '10'
-    dev_triage = '11'
-    first_resp = '12'
-    internal = '13'
+    rest = 'rest'
+    billing = 'billing'
+    customer_care = 'customer_care'
+    development = 'development'
+    dev_ateam = 'dev_ateam'
+    dev_avengers = 'dev_avengers'
+    dev_gondor = 'dev_gondor'
+    dev_mordor = 'dev_mordor'
+    dev_plugins = 'dev_plugins'
+    dev_be = 'dev_be'
+    dev_fe = 'dev_fe'
+    dev_triage = 'dev_triage'
+    first_resp = 'first_resp'
+    internal = 'internal'
 
 
 CLASS_DIRECT = {
---
+++ src/freshdesk/dataset_loader.py
@@ -0,0 +1,25 @@
+import os
+import pandas as pd
+
+
+def load_dataset(path: str = None):
+    if path is None:
+        path = 'downloads/dataset.csv'
+
+    return pd.read_csv(path)
+
+
+def filter_dataset(df):
+    df = df[(df['language'] == 'en') & (df['text'] != '') & (df['c1'] != '')]
+    return df
+
+
+if __name__ == "__main__":
+    df1 = load_dataset()
+    print(df1.shape)
+    df2 = filter_dataset(df1)
+    print(df2.shape)
+    base_path = 'downloads'
+    df2.to_csv(os.path.join(base_path, 'dataset_clean.csv'),
+               columns=['id', 'status', 'group', 'language', 'text', 'c1', 'c2', 'c3'])
+    pass
